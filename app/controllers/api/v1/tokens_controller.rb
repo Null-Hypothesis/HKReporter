3ee1d6ca4354cfcebd16ff35ce54f1d178754ab8
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 21:27:32 +0800

    feat(user): pass test for create token

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -2,6 +2,16 @@ module Api
   module V1
     class TokensController < V1Controller
       def create
+        @user = User.find_by_email(user_params[:email])
+        if @user && @user.authenticate(user_params[:password])
+          @user.update_token
+        end
+      end
+
+      private
+
+      def user_params
+        @user_params ||= params.permit(:email, :password)
       end
     end
   end
