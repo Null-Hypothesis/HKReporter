8ef03f72523685d3e60df68b9f8d433166320f3b
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 09:17:57 +0800

    feat(user): add validation to User
    fix(user): add missing presence validation for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true,
+                    uniqueness: true
+  validates :name, presence: true
+  validates :token, uniqueness: true, allow_nil: true
 end
