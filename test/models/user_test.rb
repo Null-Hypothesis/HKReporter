947035c418e07d0b341eaf9ae2d90dce6daba288
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 09:43:31 +0800

    test(user): add model test for invalid user without name
    test(user): refine invalid user without name test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,10 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(name: 'John', email: 'john@example.com')
     assert user.valid?
   end
+
+  test 'invalid user without name' do
+    user = User.new(email: 'john@example.com')
+    refute user.valid?, 'user is valid without a name'
+    refute_nil user.errors[:name], 'no validation error for name presence'
+  end
 end
