6db7b0f72c6db7042e58358ac321e9403092dc4f
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 22:53:42 +0800

    feat(user): add authenticate_token! and current_user

--- app/controllers/api/v1/v1_controller.rb
+++ app/controllers/api/v1/v1_controller.rb
@@ -1,6 +1,20 @@
 module Api
   module V1
     class V1Controller < ApplicationController
+      attr_accessor :current_user
+
+      def authenticate_token!
+        token, options = ActionController::HttpAuthentication::Token.token_and_options(request)
+
+        email = options.blank? ? nil : options[:email]
+        user = User.find_by_email email
+
+        if user && ActiveSupport::SecurityUtils.secure_compare(user.token, token)
+          self.current_user = user
+        else
+          render :unauthorized
+        end
+      end
     end
   end
 end
