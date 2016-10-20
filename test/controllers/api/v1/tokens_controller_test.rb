7f46dab2bfd302686acce6f1dde02b2909317ee2
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 21:27:17 +0800

    test(user): update test for create token

--- test/controllers/api/v1/tokens_controller_test.rb
+++ test/controllers/api/v1/tokens_controller_test.rb
@@ -4,7 +4,13 @@ module Api
   module V1
     class TokensControllerTest < ActionDispatch::IntegrationTest
       test 'should create token' do
-        post api_v1_token_url
+        john = users(:john)
+        post api_v1_token_url,
+             params: {
+               email: john.email,
+               password: '123123'
+             }
+
         assert_response :success
       end
     end
