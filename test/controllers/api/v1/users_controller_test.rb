41dc013e2aa76a858b685de02730fac75cdf0a44
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 14:45:04 +0800

    test(user): update create user test for count difference

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -4,15 +4,17 @@ module Api
   module V1
     class UsersControllerTest < ActionDispatch::IntegrationTest
       test 'should create new user' do
-        post api_v1_users_url,
-             params: {
-               email: 'chairman@example.com',
-               password: 'hahaha',
-               passwordConfirmation: 'hahaha',
-               name: 'Haha',
-               description: 'chairman'
-             },
-             as: :json
+        assert_difference('User.count') do
+          post api_v1_users_url,
+               params: {
+                 email: 'chairman@example.com',
+                 password: 'hahaha',
+                 passwordConfirmation: 'hahaha',
+                 name: 'Haha',
+                 description: 'chairman'
+               },
+               as: :json
+        end
         assert_response :success
       end
     end
