d19ee972b1b231117e0ca392de2269ff171e0eb4
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 15:15:55 +0800

    refactor(user): extract user_params in users controller test

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -3,18 +3,25 @@ require 'test_helper'
 module Api
   module V1
     class UsersControllerTest < ActionDispatch::IntegrationTest
+      attr_accessor :user_params
+
+      def setup
+        @user_params = {
+          email: 'chairman@example.com',
+          password: 'hahaha',
+          passwordConfirmation: 'hahaha',
+          name: 'Haha',
+          description: 'chairman'
+        }
+      end
+
       test 'should create new user' do
         assert_difference('User.count') do
           post api_v1_users_url,
-               params: {
-                 email: 'chairman@example.com',
-                 password: 'hahaha',
-                 passwordConfirmation: 'hahaha',
-                 name: 'Haha',
-                 description: 'chairman'
-               },
+               params: user_params,
                as: :json
         end
+
         assert_response :success
       end
     end
