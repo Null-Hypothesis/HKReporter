2d806f65e03b17d98961882387d3b9f86bf9edae
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 10:04:50 +0800

    refactor(user): extract user to @user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,20 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: 'John', email: 'john@example.com')
+  end
+
   test 'valid user' do
-    user = User.new(name: 'John', email: 'john@example.com')
-    assert user.valid?
+    assert @user.valid?
   end
 
   test 'invalid user without name' do
-    user = User.new(email: 'john@example.com')
-    refute user.valid?, 'user is valid without a name'
-    refute_nil user.errors[:name], 'no validation error for name presence'
+    @user.name = nil
+    refute @user.valid?, '@user is valid without a name'
+    refute_nil @user.errors[:name], 'no validation error for name presence'
   end
 
   test 'invalid user without email' do
-    user = User.new(name: 'John')
-    refute user.valid?, 'user is valid without a email'
-    refute_nil user.errors[:email], 'no validation error for email presence'
+    @user.email = nil
+    refute @user.valid?, '@user is valid without a email'
+    refute_nil @user.errors[:email], 'no validation error for email presence'
   end
 end
