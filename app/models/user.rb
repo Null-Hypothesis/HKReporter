ec35ee8ae39f94dd7d7de8cdb61dc30d8ca2efec
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 09:57:43 +0800

    feat(user): add update_token method
    fix(user): fix new_valid_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
                     uniqueness: true
   validates :name, presence: true
   validates :token, uniqueness: true, allow_nil: true
+
+  def update_token
+    update!(token: new_valid_token)
+  end
+
+  private
+
+  def new_valid_token
+    loop do
+      token = SecureRandom.uuid
+      return token unless self.class.exists?(token: token)
+    end
+  end
 end
