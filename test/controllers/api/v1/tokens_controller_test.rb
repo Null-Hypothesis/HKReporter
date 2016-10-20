468cb43f1a0930d35e44ceefa187219a7ab807ba
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 21:31:53 +0800

    test(user): add view test for create token

--- test/controllers/api/v1/tokens_controller_test.rb
+++ test/controllers/api/v1/tokens_controller_test.rb
@@ -9,9 +9,14 @@ module Api
              params: {
                email: john.email,
                password: '123123'
-             }
+             },
+             as: :json
 
         assert_response :success
+
+        assert response_json['id']
+        assert response_json['token']
+        assert_equal john.email, response_json['email']
       end
     end
   end
