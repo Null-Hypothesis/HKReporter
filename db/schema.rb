c2ba4b57f6ec104b4fa7a8ffb5a9f7022a29a2c3
Yiming Chen <dsdshcym@gmail.com>
2016-10-20 22:57:05 +0800

    feat(course): generate model Course

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020062804) do
+ActiveRecord::Schema.define(version: 20161020145537) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "course_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
