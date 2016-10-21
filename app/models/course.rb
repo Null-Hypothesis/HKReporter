0e497240243b0c45c1b6df69770786ef9eab3da5
Yiming Chen <dsdshcym@gmail.com>
2016-10-21 09:17:09 +0800

    feat(course): add has_many through association for course and tag

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+  has_many :course_taggings
+  has_many :course_tags, through: :course_taggings
 end
--- app/models/course_tag.rb
+++ app/models/course_tag.rb
@@ -1,2 +1,4 @@
 class CourseTag < ApplicationRecord
+  has_many :course_taggings
+  has_many :courses, through: :course_taggings
 end
