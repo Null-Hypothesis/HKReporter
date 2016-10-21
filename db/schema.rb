fbd9abcdfbb92126ff9d728869d598aacb4045d7
Yiming Chen <dsdshcym@gmail.com>
2016-10-21 09:18:20 +0800

    feat(course): generate model Teacher

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021011210) do
+ActiveRecord::Schema.define(version: 20161021011807) do
 
   create_table "course_taggings", force: :cascade do |t|
     t.integer  "course_id"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20161021011210) do
     t.string   "course_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
