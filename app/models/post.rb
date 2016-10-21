52dc9b997afaf532e6e20f609ba72fb0e61a7b3c
Yiming Chen <dsdshcym@gmail.com>
2016-10-21 09:38:09 +0800

    feat(post): add has_many through association for Post and PostTag

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :course
   belongs_to :user
+
+  has_many :post_taggings
+  has_many :post_tags, through: :post_taggings
 end
--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,2 +1,4 @@
 class PostTag < ApplicationRecord
+  has_many :post_taggings
+  has_many :post, through: :post_taggings
 end
