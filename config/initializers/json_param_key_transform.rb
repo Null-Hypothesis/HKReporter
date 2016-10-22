6630e059c7ccbd2d794e7620174fee6256a26dfc
Yiming Chen <dsdshcym@gmail.com>
2016-10-22 15:56:20 +0800

    fix(params): fix json_param_key_transform (again)

--- config/initializers/json_param_key_transform.rb
+++ config/initializers/json_param_key_transform.rb
@@ -1,4 +1,5 @@
 # See http://stackoverflow.com/questions/17240106/what-is-the-best-way-to-convert-all-controller-params-from-camelcase-to-snake-ca
+# And parameter_parsers doc
 
 # Transform JSON request param keys from JSON-conventional camelCase to
 # Rails-conventional snake_case:
@@ -6,5 +7,5 @@ ActionDispatch::Request.parameter_parsers[:json] = lambda do |raw_post|
   # Modified from action_dispatch/http/parameters.rb
   data = ActiveSupport::JSON.decode(raw_post)
 
-  data.is_a?(Hash) ? data.deep_transform_keys!(&:underscore) : data
+  data.is_a?(Hash) ? data.deep_transform_keys!(&:underscore) : { _json: data }
 end
