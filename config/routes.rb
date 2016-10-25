560acfe12d9c8108c3e3d92f21943670fab8dbea
Yiming Chen <dsdshcym@gmail.com>
2016-10-25 16:10:07 +0800

    fix(routes): set default format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i(create) do
         collection do
