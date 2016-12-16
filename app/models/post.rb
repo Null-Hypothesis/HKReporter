acc8287d99d488436d9b1872e2ab3ef20adc210d
Yiming Chen <dsdshcym@gmail.com>
2016-12-16 15:21:35 +0800

    fix(favorites): add dependent: :destroy to post
    - fix posts#destroy test

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ApplicationRecord
   has_many :post_taggings
   has_many :post_tags, through: :post_taggings
   has_many :replies, dependent: :destroy
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
 end
